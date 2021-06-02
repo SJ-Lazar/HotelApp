98c0b76b374b70bb6ced385c726bd792aa2c592f
SJ-Lazar <78497706+SJ-Lazar@users.noreply.github.com>
2021-06-02 11:44:20 +0200

    Added SqlDataAccess Class
    A data access class with two methods:
    LoadData and SaveData That take in sql statements.
    Extracted a inteface for sqlDataAccess.

---
+++ HotelAppDataAccessLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelAppDataAccessLibrary.Databases
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlDataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement,
+                                      U parameters,
+                                      string connectionStringName,
+                                      bool isStoredProcedure = false)
+        {
+            string connectionString = _configuration.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            //Anonymous class checking if is  stored procedure than assign to commandType it the type of stored procedure
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters, commandType: commandType).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement,
+                                T parameters,
+                                string connectionStringName,
+                                bool isStoredProcedure = false)
+        {
+            string connectionString = _configuration.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            //Anonymous class checking if is  stored procedure than assign to commandType it the type of stored procedure
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters, commandType: commandType);
+            }
+        }
+    }
+}
