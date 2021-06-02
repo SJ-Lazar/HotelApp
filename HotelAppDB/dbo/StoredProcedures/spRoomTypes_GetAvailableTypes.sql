8a3731b4ea1107916198b5439b6c07a28a093792
SJ-Lazar <78497706+SJ-Lazar@users.noreply.github.com>
2021-06-02 13:50:16 +0200

    Added SqlData Class
    sqlDataClass Added Method GetAvailableRoomTypes

---
+++ HotelAppDB/dbo/StoredProcedures/spRoomTypes_GetAvailableTypes.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[spRoomTypes_GetAvailableTypes]
+	@startDate date,
+	@endDate date
+
+
+AS
+BEGIN
+
+	--Dont return rows that are affected
+	SET NOCOUNT ON;
+
+	SELECT t.Id, t.Title, t.[Description ], t.Price
+	FROM dbo.Rooms r
+
+	--Join room table to roomtypes table
+	INNER JOIN dbo.RoomTypes t 
+	ON t.Id = r.RoomTypeId
+	WHERE r.Id NOT IN (
+	SELECT b.RoomId 
+	FROM dbo.Bookings b
+
+	--Checking for rooms types that are avaliable that dont have bookings bookings 
+	WHERE (@StartDate < b.StartDate AND @endDate > b.EndDate)
+	OR (b.StartDate <= @endDate AND @endDate < b.EndDate)
+	OR (b.StartDate <= @startDate AND @startDate < b.StartDate)
+	)
+
+	--Remove duplicate 
+	GROUP BY t.Id, t.Title, t.[Description ], t.Price
+
+END
---
+++ HotelAppDataAccessLibrary/Data/SqlData.cs
@@ -0,0 +1,33 @@
+﻿using HotelAppDataAccessLibrary.Databases;
+using HotelAppDataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAppDataAccessLibrary.Data
+{
+   
+    public class SqlData
+    {
+        private readonly ISqlDataAccess _sqlDataAccess;
+        private const string connectionStringName = "SqlDataBase";
+
+        public SqlData(ISqlDataAccess sqlDataAccess)
+        {
+            _sqlDataAccess = sqlDataAccess;
+        }
+
+        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+           return _sqlDataAccess.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",
+                                                            new { startDate, endDate },
+                                                            connectionStringName,
+                                                            true);
+        }
+
+        
+
+    }
+}
